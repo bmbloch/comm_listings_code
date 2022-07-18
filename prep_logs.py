9e6a0bb0a9131f95f6cd7466bef79c6acdd49582
Ben Bloch <ben.bloch@moodys.com>
2022-07-18 13:58:03 -0400

    correcting field names, and adding clause to drop props with year builts in the future when they are net new props

--- prep_logs.py
+++ prep_logs.py
@@ -1303,11 +1303,12 @@ class PrepareLogs:
             test_data['include'] = False
             test_data['include'] = np.where((test_data['subcategory'] == 'mixed_use') & (test_data[size_by_use] > 0), True, test_data['include'])
             test_data['include'] = np.where((~test_data['category'].isin(self.sector_map[self.sector]['category'])) | (~test_data['subcategory'].isin(self.sector_map[self.sector]['subcategory'] + [''])), False, test_data['include'])
-            test_data['include'] = np.where((test_data['subcategory'] == 'warehouse_office') & (self.sector == 'ind') & (test_data['building_industrial_use_size_sf'].isnull() == True) & (test_data['building_office_use_size_sf'].isnull() == True), False, test_data['include'])
+            test_data['include'] = np.where((test_data['subcategory'] == 'warehouse_office') & (self.sector == 'ind') & (test_data['building_industrial_size_sf'].isnull() == True) & (test_data['building_office_size_sf'].isnull() == True), False, test_data['include'])
             test_data['include'] = np.where((test_data['buildings_condominiumized_flag'] == 'Y'), False, test_data['include'])
             test_data['include'] = np.where(((test_data['retail_center_type'] != '') | (test_data['subcategory'] == '')) & (self.sector == 'ret'), False, test_data['include'])
             test_data['include'] = np.where((self.sector in ['off', 'ind']) & (test_data['tot_size'] < 10000), False, test_data['include'])
             test_data['include'] = np.where((test_data['property_geo_msa_code'] == '') | (test_data['property_geo_subid'].isnull() == True), False, test_data['include'])
+            test_data['include'] = np.where((test_data['first_year'] > self.curryr) | ((test_data['first_year'] == self.curryr) & (test_data['buildings_construction_expected_completion_month'] > self.currmon)), False, test_data['include'])
             test_data['include'] = np.where((test_data['legacy']), True, test_data['include'])
             
             temp = test_data.copy()
@@ -1317,7 +1318,7 @@ class PrepareLogs:
                 self.drop_log = self.drop_log.append(temp[['property_source_id', 'id_use', 'reason']].drop_duplicates('property_source_id'), ignore_index=True)
             test_data = test_data[(test_data['include'])]
             if self.sector == 'ind':
-                test_data['off_perc'] = np.where((test_data['building_office_use_size_sf'].isnull() == False), test_data['building_office_use_size_sf'] / test_data['size'], (test_data['size'] - test_data['building_industrial_use_size_sf']) / test_data['size'])
+                test_data['off_perc'] = np.where((test_data['building_office_size_sf'].isnull() == False), test_data['building_office_size_sf'] / test_data['size'], (test_data['size'] - test_data['building_industrial_size_sf']) / test_data['size'])
                 test_data['subcategory'] = np.where((test_data['leg'] == False) & (test_data['off_perc'] >= 0.25), 'warehouse_flex', test_data['subcategory'])
                 test_data['subcategory'] = np.where((test_data['leg'] == False) & (test_data['off_perc'] < 0.25), 'warehouse_distribution', test_data['subcategory'])
                 test_data['type2'] = np.where((test_data['leg'] == False) & (test_data['subcategory'] == 'warehouse_distribution'), 'W', test_data['type2'])
