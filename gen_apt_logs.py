fe14b841593b4776b7cf1be464a31efee0e8cfe8
Ben Bloch <ben.bloch@moodys.com>
2022-07-19 10:04:24 -0400

    will comment this clause out until we can be confident that the data is in order

--- gen_apt_logs.py
+++ gen_apt_logs.py
@@ -197,12 +197,13 @@ drop_log = drop_log.append(temp.drop_duplicates('property_source_id')[['property
 del temp
 df = df[(df['year'] < curryr) | (df['year'].isnull() == True) | ((df['year'] == curryr) & ((df['month'] <= currmon) | (df['month'].isnull() == True)))]
 
-temp = df.copy()
-temp = temp[temp['category'] != 'multifamily']
-temp['reason'] = 'property no longer multifamily'
-drop_log = drop_log.append(temp.drop_duplicates('property_source_id')[['property_source_id', 'property_reis_rc_id', 'reason']], ignore_index=True)
-del temp
-df = df[df['category'] == 'multifamily']
+print("Put this in once we are confident that data is clean")
+# temp = df.copy()
+# temp = temp[temp['category'] != 'multifamily']
+# temp['reason'] = 'property no longer multifamily'
+# drop_log = drop_log.append(temp.drop_duplicates('property_source_id')[['property_source_id', 'property_reis_rc_id', 'reason']], ignore_index=True)
+# del temp
+# df = df[df['category'] == 'multifamily']
 
 if len(df[df['survdate'].isnull() == True]) > 0:
     print("There are rows that are missing a survey date")
