7c33cb980c5e7706ad6d702303f7875218b6c5e5
Ben Bloch <ben.bloch@moodys.com>
2022-07-28 17:25:38 -0400

    need to check that er_id is actually in the log before doing this

--- prep_logs.py
+++ prep_logs.py
@@ -3198,33 +3198,37 @@ class PrepareLogs:
                 for er_id in er_ids:
                     if er_id[0] == self.sector_map[self.sector]['prefix']:
                         
-                        propname = row['property_name']
-                        er_propname = log_dict[er_id[1:]]
-                        test1 = 'bldg' in propname.lower() or 'building' in propname.lower()
-                        test2 = 'bldg' in er_propname.lower() or 'building' in er_propname.lower()
-                        test3 = propname.split('bldg')[-1].strip().split(' ')[0].isdigit() 
-                        test4 = propname.split('building')[-1].strip().split(' ')[0].isdigit()
-                        test5 = er_propname.split('bldg')[-1].strip().split(' ')[0].isdigit() 
-                        test6 = er_propname.split('building')[-1].strip().split(' ')[0].isdigit()
-                        if test3:
-                            num = propname.split('bldg')[-1].strip().split(' ')[0]
-                        elif test4:
-                            num = propname.split('building')[-1].strip().split(' ')[0]
-                        else:
-                            num = '1'
-                        if test5:
-                            er_num = er_propname.split('bldg')[-1].strip().split(' ')[0]
-                        elif test6:
-                            er_num = er_propname.split('building')[-1].strip().split(' ')[0]
-                        else:
-                            er_num = '1'
-                        test7 = num != er_num
+                        if er_id[1:] in list(log_dict.keys()):
                         
-                        if test1 and test2 and (test3 or test4) and (test5 and test6) and test7:
-                            building_test = True
+                            propname = row['property_name']
+                            er_propname = log_dict[er_id[1:]]
+                            test1 = 'bldg' in propname.lower() or 'building' in propname.lower()
+                            test2 = 'bldg' in er_propname.lower() or 'building' in er_propname.lower()
+                            test3 = propname.split('bldg')[-1].strip().split(' ')[0].isdigit() 
+                            test4 = propname.split('building')[-1].strip().split(' ')[0].isdigit()
+                            test5 = er_propname.split('bldg')[-1].strip().split(' ')[0].isdigit() 
+                            test6 = er_propname.split('building')[-1].strip().split(' ')[0].isdigit()
+                            if test3:
+                                num = propname.split('bldg')[-1].strip().split(' ')[0]
+                            elif test4:
+                                num = propname.split('building')[-1].strip().split(' ')[0]
+                            else:
+                                num = '1'
+                            if test5:
+                                er_num = er_propname.split('bldg')[-1].strip().split(' ')[0]
+                            elif test6:
+                                er_num = er_propname.split('building')[-1].strip().split(' ')[0]
+                            else:
+                                er_num = '1'
+                            test7 = num != er_num
+                            
+                            if test1 and test2 and (test3 or test4) and (test5 and test6) and test7:
+                                building_test = True
+                            else:
+                                building_test = False
                         else:
                             building_test = False
-                        
+                            
                         if er_id[1:] in list(log_dict.keys()) and (nc_link not in er_ids or nc_link[1:] in list(log_dict.keys())) and not building_test:
                             drop_list.append(row['property_source_id'])
                             dropped = True
